362dd0f91c3ab4d74d8db32df8404712bead542e
richclement <richclement@gmail.com>
2015-08-02 12:16:08 -0400

    update to instructions and ignore files

--- Web/wwwroot/src/child-router.js
+++ Web/wwwroot/src/child-router.js
@@ -5,6 +5,7 @@ export class ChildRouter{
     config.map([
       { route: ['','welcome'], name: 'welcome',      moduleId: './welcome',      nav: true, title:'Welcome' },
       { route: 'users',       name: 'users',       moduleId: './users',       nav: true, title:'Github Users' },
+      { route: 'values',       name: 'values',       moduleId: './values',       nav: true, title:'Values' },
       { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true, title:'Child Router' }
     ]);
 
